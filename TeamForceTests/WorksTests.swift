3c319e5f183e7eda5f9eae4f4bf01923938407a9
AlexSolo <setixela@gmail.com>
2022-08-12 06:19:38 +0300

    some test

---
+++ TeamForceTests/WorksTests.swift
@@ -0,0 +1,79 @@
+//
+//  TeamForceTests.swift
+//  TeamForceTests
+//
+//  Created by Aleksandr Solovyev on 17.06.2022.
+//
+
+import ReactiveWorks
+@testable import TeamForce
+import XCTest
+
+class WorksTests: XCTestCase {
+   func testSyncWork() {
+      let work = Work<Int, Int>(input: 10) {
+         $0.success(result: $0.unsafeInput + 1)
+      }
+      work.doSync()
+
+      XCTAssertEqual(work.result, 11)
+   }
+
+   func testAsyncWork() {
+      let exp = expectation(description: "")
+      let work = Work<Int, Int>(input: 10) {
+         $0.success(result: $0.unsafeInput + 1)
+         exp.fulfill()
+      }
+      work.doAsync()
+
+      wait(for: [exp], timeout: 1)
+
+      XCTAssertEqual(work.result, 11)
+   }
+
+   func testWorkDoMixIsNotCatchObj() {
+      var testObj: NSObject? = NSObject()
+      weak var testObjCheck = testObj
+      let exp = expectation(description: "")
+      let work = Work<Int, Int>(input: 10) {
+         $0.success(result: $0.unsafeInput + 1)
+      }
+      work
+         .doMix(testObj)
+         .onSuccess { _, _ in
+            fatalError()
+         }.onFail {
+            exp.fulfill()
+         }
+      testObj = nil
+      work.doAsync()
+
+      wait(for: [exp], timeout: 1)
+
+      XCTAssertNil(testObjCheck)
+   }
+
+   func testWorkDoInputIsNotCatchObj() {
+      var testObj: NSObject? = NSObject()
+      weak var testObjCheck = testObj
+      let exp = expectation(description: "")
+      let work = Work<Int, Int>(input: 10) {
+         $0.success(result: $0.unsafeInput + 1)
+      }
+      work
+         .doInput(testObj)
+         .onSuccess { _ in
+            fatalError()
+         }.onFail {
+            exp.fulfill()
+         }
+      testObj = nil
+      work.doAsync()
+
+      wait(for: [exp], timeout: 1)
+
+      XCTAssertNil(testObjCheck)
+   }
+
+}
