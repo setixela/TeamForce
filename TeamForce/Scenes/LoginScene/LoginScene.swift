e04d997a2844a019ca54879a45a9942280be59d0
AlexSolo <setixela@gmail.com>
2022-08-16 03:48:46 +0300

    wip

--- TeamForce/Scenes/LoginScene/LoginScene.swift
+++ TeamForce/Scenes/LoginScene/LoginScene.swift
@@ -160,36 +160,6 @@ private extension LoginScene {
    }
 }
 
-final class DoubleStacksBrandedVM<Design: DesignProtocol>: Combos<SComboMD<StackModel, StackModel>>,
-   Designable
-{
-   lazy var header = Design.label.headline5
-      .set_color(Design.color.textInvert)
-
-   required init() {
-      super.init()
-
-      setMain {
-         $0
-            .set(Design.state.stack.default)
-            .set_backColor(Design.color.backgroundBrand)
-            .set_alignment(.leading)
-            .set_models([
-               Grid.x16.spacer,
-               BrandLogoIcon<Design>(),
-               Grid.x16.spacer,
-               header,
-               Grid.x36.spacer
-            ])
-      } setDown: {
-         $0
-            .set_backColor(Design.color.background)
-            .set_padding(.top(-Grid.x16.value))
-            .set_padBottom(-Grid.x16.value)
-      }
-   }
-}
-
 // MARK: - Configure scene states
 
 private extension LoginScene {
