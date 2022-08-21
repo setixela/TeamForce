583c466e6b8e43acea5f5912b1d203d69a112084
AlexSolo <setixela@gmail.com>
2022-08-21 06:04:10 +0300

    fixings

--- TeamForce/Networking/ApiEngine.swift
+++ TeamForce/Networking/ApiEngine.swift
@@ -9,60 +9,62 @@ import Foundation
 import PromiseKit
 
 enum ApiEngineError: Error {
-    case unknown
-    case error(Error)
+   case unknown
+   case error(Error)
 }
 
 struct ApiEngineResult {
-    let data: Data?
-    let response: URLResponse?
+   let data: Data?
+   let response: URLResponse?
 }
 
 final class ApiEngine: ApiEngineProtocol {
-    func process(endpoint: EndpointProtocol) -> Promise<ApiEngineResult> {
-        return Promise { seal in
-            guard let url = URL(string: endpoint.endPoint) else {
-                seal.reject(ApiEngineError.unknown)
-                return
+   func process(endpoint: EndpointProtocol) -> Promise<ApiEngineResult> {
+      return Promise { seal in
+         guard let url = URL(string: endpoint.endPoint) else {
+            seal.reject(ApiEngineError.unknown)
+            return
+         }
+
+         let method = endpoint.method
+         let params = endpoint.body
+         let headers = endpoint.headers
+
+         var request = URLRequest(url: url)
+
+         request.httpMethod = method.rawValue
+
+         for (key, value) in headers {
+            request.setValue(value, forHTTPHeaderField: key)
+         }
+
+         request.httpBody = params
+            .map { (key: String, value: Any) in
+               key + "=\(value)"
+            }
+            .joined(separator: "&")
+            .data(using: .utf8)
+
+         let task = URLSession.shared.dataTask(with: request) { data, response, error in
+
+            guard let data = data else {
+               guard let error = error else {
+                  seal.reject(ApiEngineError.unknown)
+                  print("\nResponse:\n\(String(describing: response))\n\nData:\n\(String(describing: data))\n\nError:\n\(String(describing: error)))")
+                  return
+               }
+
+               print("\nResponse:\n\(String(describing: response))\n\nData:\n\(String(describing: data))\n\nError:\n\(String(describing: error)))")
+               print(error)
+               seal.reject(ApiEngineError.error(error))
+               return
             }
 
-            let method = endpoint.method
-            let params = endpoint.body
-            let headers = endpoint.headers
+            let apiResult = ApiEngineResult(data: data, response: response)
+            seal.fulfill(apiResult)
+         }
 
-            var request = URLRequest(url: url)
-
-            request.httpMethod = method.rawValue
-
-            for (key, value) in headers {
-                request.setValue(value, forHTTPHeaderField: key)
-            }
-
-            request.httpBody = params
-                .map { (key: String, value: Any) in
-                    key + "=\(value)"
-                }
-                .joined(separator: "&")
-                .data(using: .utf8)
-
-            let task = URLSession.shared.dataTask(with: request) { data, response, error in
-                print("\nResponse:\n\(String(describing: response))\n\nData:\n\(String(describing: data))\n\nError:\n\(String(describing: error)))")
-                guard let data = data else {
-                    guard let error = error else {
-                        seal.reject(ApiEngineError.unknown)
-                        return
-                    }
-
-                    print(error)
-                    seal.reject(ApiEngineError.error(error))
-                    return
-                }
-
-                let apiResult = ApiEngineResult(data: data, response: response)
-                seal.fulfill(apiResult)
-            }
-
-            task.resume()
-        }
-    }
+         task.resume()
+      }
+   }
 }
--- TeamForce/Scenes/LoginScene/LoginScenario.swift
+++ TeamForce/Scenes/LoginScene/LoginScenario.swift
@@ -24,54 +24,17 @@ final class LoginScenario<Asset: AssetProtocol>:
    >
 {
    override func start() {
-      weak var slf = self
-
       // setup input field reactions
       events.userNameStringEvent
-         .onSuccess {
-            // weakSelf?.badgeModel.changeState(to: BadgeState.default)
-         }
          .doNext(work: works.loginNameInputParse)
-         .onSuccess {
-            slf?.setState(.nameInputParseSuccess($0))
-         }
-         .onFail { (text: String) in
-            slf?.setState(.smsInputParseError(text))
-         }
+         .onSuccess(setState) { .nameInputParseSuccess($0) }
+         .onFail(setState) { .smsInputParseError($0) }
 
       // setup get code button reaction
       events.getCodeButtonEvent
          .doNext(work: works.authByName)
-         .onSuccess {
-            slf?.setState(.inputSmsCode)
-         }
-         .onFail {
-            slf?.setState(.inputUserName)
-         }
+         .onSuccess(setState, .inputSmsCode)
 
-      // setup input field reactions
-      events.smsCodeStringEvent
-         //
-         .doNext(work: works.smsCodeInputParse)
-         .onSuccess {
-            slf?.setState(.smsInputParseSuccess($0))
-         }.onFail { (text: String) in
-            slf?.setState(.smsInputParseError(text))
-         }
-
-      // setup login button reactions
-      events.loginButtonEvent
-         //
-         .doNext(work: works.verifyCode)
-         .onFail {
-            print("Verify api error")
-         }
-         .doNext(work: works.saveLoginResults)
-         .onSuccess {
-            Asset.router?.route(\.main, navType: .present, payload: ())
-         }
-         .onFail {
-            print("Save login results to persistence error")
-         }
+7
    }
 }
