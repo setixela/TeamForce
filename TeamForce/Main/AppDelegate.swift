66da06d2fab7152a687fbd31d306a7fa6adb4081
Yerzhan <gapurinoverzhan@gmail.com>
2022-11-10 00:22:54 +0500

    FCM token and notifications

--- TeamForce/Main/AppDelegate.swift
+++ TeamForce/Main/AppDelegate.swift
@@ -7,17 +7,45 @@
 
 import UIKit
 import Firebase
+import FirebaseCore
+import FirebaseMessaging
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+   
+   let gcmMessageIDKey = "gcm.Message_ID"
+   
    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
       // Override point for customization after application launch.
       UserDefaults.standard.set(false, forKey: "_UIConstraintBasedLayoutLogUnsatisfiable")
 
       FirebaseApp.configure()
+      
+      if #available(iOS 10.0, *) {
+        // For iOS 10 display notification (sent via APNS)
+        UNUserNotificationCenter.current().delegate = self
+
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+      } else {
+        let settings: UIUserNotificationSettings =
+          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+        application.registerUserNotificationSettings(settings)
+      }
+
+      application.registerForRemoteNotifications()
+      
+      // Messaging delegate
+      Messaging.messaging().delegate = self
+
 
       return true
    }
+   
+   
 
    // MARK: UISceneSession Lifecycle
 
@@ -32,4 +60,80 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
       // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
    }
+}
+
+
+//MARK: - UNUserNotificationCenterDelegate
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+   // Receive displayed notifications for iOS 10 devices.
+   func userNotificationCenter(_ center: UNUserNotificationCenter,
+                               willPresent notification: UNNotification) async
+      -> UNNotificationPresentationOptions
+   {
+      let userInfo = notification.request.content.userInfo
+
+      // With swizzling disabled you must let Messaging know about the message, for Analytics
+      // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+      // ...
+
+      // Print full message.
+      print(userInfo)
+
+      // Change this to your preferred presentation option
+      return [[.alert, .sound]]
+   }
+
+   func userNotificationCenter(_ center: UNUserNotificationCenter,
+                               didReceive response: UNNotificationResponse) async
+   {
+      let userInfo = response.notification.request.content.userInfo
+
+      // ...
+
+      // With swizzling disabled you must let Messaging know about the message, for Analytics
+      // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+      // Print full message.
+      print(userInfo)
+   }
+   
+   func application(_ application: UIApplication,
+                    didReceiveRemoteNotification userInfo: [AnyHashable: Any]) async
+     -> UIBackgroundFetchResult {
+     // If you are receiving a notification message while your app is in the background,
+     // this callback will not be fired till the user taps on the notification launching the application.
+     // TODO: Handle data of notification
+
+     // With swizzling disabled you must let Messaging know about the message, for Analytics
+     // Messaging.messaging().appDidReceiveMessage(userInfo)
+
+     // Print message ID.
+     if let messageID = userInfo[gcmMessageIDKey] {
+       print("Message ID: \(messageID)")
+     }
+
+     // Print full message.
+     print(userInfo)
+
+     return UIBackgroundFetchResult.newData
+   }
+
+}
+
+extension AppDelegate: MessagingDelegate {
+   func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+     print("Firebase registration token: \(String(describing: fcmToken))")
+
+     let dataDict: [String: String] = ["token": fcmToken ?? ""]
+     NotificationCenter.default.post(
+       name: NSNotification.Name("FCMToken"),
+       object: nil,
+       userInfo: dataDict
+     )
+     // TODO: If necessary send token to application server.
+     // Note: This callback is fired at each app startup and whenever a new token is generated.
+   }
+
 }
